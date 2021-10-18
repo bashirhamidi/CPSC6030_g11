b0838a56951d9f991356efee0e9a583f46ec3ba9
Mark B Hamidi <28765863+bashirhamidi@users.noreply.github.com>
2021-10-17 23:31:03 -0400

    Update figure_1.js

--- figure_1.js
+++ figure_1.js
@@ -1,9 +1,10 @@
 d3
-var circle = elemEnter.append("circle")
-                          .attr("cx",50)
-                          .attr("cy",50)
-                          .attr("r",10)
-                          .style("fill","red");
+svg.append('circle')
+  .attr('cx', 100)
+  .attr('cy', 100)
+  .attr('r', 50)
+  .attr('stroke', 'black')
+  .attr('fill', '#69a3b2');
 
 console.log()
 
