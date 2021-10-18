c35bd21819c50735828e519fae3cc6fcbdfd3513
Mark B Hamidi <28765863+bashirhamidi@users.noreply.github.com>
2021-10-17 23:33:13 -0400

    Update figure_1.js

--- figure_1.js
+++ figure_1.js
@@ -1,4 +1,8 @@
 d3
+// create svg element:
+var svg = d3.select("#circle").append("svg").attr("width", 200).attr("height", 200)
+
+// Add the path using this helper function
 svg.append('circle')
   .attr('cx', 100)
   .attr('cy', 100)
