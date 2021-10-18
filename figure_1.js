4e82ddfe514b290a23b70e707f33de15c270792b
Mark B Hamidi <28765863+bashirhamidi@users.noreply.github.com>
2021-10-17 23:09:39 -0400

    Update figure_1.js

--- figure_1.js
+++ figure_1.js
@@ -1,5 +1,4 @@
-<script>
 d3
   .select(".target")  // select the elements that have the class 'target'
   .style("stroke-width", 8) // change their style: stroke width is not equal to 8 pixels
-</script>
+
