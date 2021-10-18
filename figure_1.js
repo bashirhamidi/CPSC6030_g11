361d16d239e8ab02955b2bca87f5ae25df3b8525
Mark B Hamidi <28765863+bashirhamidi@users.noreply.github.com>
2021-10-17 23:07:09 -0400

    Create figure_1.js

---
+++ figure_1.js
@@ -0,0 +1,5 @@
+<script>
+d3
+  .select(".target")  // select the elements that have the class 'target'
+  .style("stroke-width", 8) // change their style: stroke width is not equal to 8 pixels
+</script>
