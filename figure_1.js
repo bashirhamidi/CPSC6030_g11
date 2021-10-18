9b7301ef76d3470a15cf315278b41f2f1ea98b7d
Mark B Hamidi <28765863+bashirhamidi@users.noreply.github.com>
2021-10-17 23:28:13 -0400

    Update figure_1.js

--- figure_1.js
+++ figure_1.js
@@ -1,7 +1,9 @@
 d3
-<svg width="500" height="500">
-    <rect x="0" y="0" width="200" height="200"></rect>
-</svg>
+var circle = elemEnter.append("circle")
+                          .attr("cx",50)
+                          .attr("cy",50)
+                          .attr("r",10)
+                          .style("fill","red");
 
 console.log()
 
