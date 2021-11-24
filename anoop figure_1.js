cd225e556f219ad54a8132548d5469cb36772eed
anoopkak <90483777+anoopkak@users.noreply.github.com>
2021-11-24 14:00:24 -0500

    Add files via upload

--- anoop figure_1.js
+++ anoop figure_1.js
@@ -2,16 +2,16 @@ d3.json("18245_madrid.json").then(function(dataset){
     
     var jersey_numbers={5597 : 1, 5721 : 2, 5485 : 5, 5201 : 4, 5552 : 12, 5539 : 14, 5463 : 10, 5574 : 8, 4926: 22, 19677 : 9, 5207 : 7}
         
-    console.log(jersey_numbers[5552])
+    console.log(dataset)
 
-    var plotscale = 900
+    var plotscale = 800
     
     var margin = {top: (plotscale * (14.86/960)), 
                     right: (plotscale * (20/960)), 
                     bottom: (plotscale * (24/960)), 
-                    left: (plotscale* (40/960))},
-        width = plotscale - margin.left - margin.right,
-        height = (plotscale * (68/105) - margin.top - margin.bottom) 
+                    left: (plotscale* (40/960))}
+    var width = plotscale - margin.left - margin.right
+    var height = (plotscale * (68/105) - margin.top - margin.bottom) 
         console.log(68/105, height/width)
     
     var x = d3.scaleLinear()
@@ -22,11 +22,12 @@ d3.json("18245_madrid.json").then(function(dataset){
         .domain([0, 80])
         .range([0,height])
     
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    var svg = d3.select("#pattern")
+                //.append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
     
     svg.append("clipPath")
         .attr("id", "clip")
@@ -120,13 +121,30 @@ d3.json("18245_madrid.json").then(function(dataset){
      
     //group_data=Array.from(d3.group(dataset, d => d.type.name))
       //console.log(dataset)
-    console.log(dataset)
+    
     //console.log(d3.group(dataset, d=>d.location))
     var start = Array.from(d3.group(dataset, d => d.location))
-    console.log(start) 
+    
     //var start= d3.group(dataset, d=>d.location)
     var end=Array.from(d3.group(dataset, d=>d.pass.end_location))
-    console.log(dataset)
+    
+
+    var temp=10
+    var edges = svg.append("g")
+                        .selectAll("line")
+                        .data(dataset)
+                        .enter()
+                        .append("line")
+                        .attr("x1", function(d){return x(d.location[0])})                           
+                        .attr("y1", function(d){return y(d.location[1])})
+                        .attr("x2", function(d){return x(d.pass.end_location[0])})
+                        .attr("y2", function(d){return y(d.pass.end_location[1])})
+                        .attr("stroke", "black")
+                        .attr("stroke-width", function(d){
+                            if (d.minute==temp && (d.pass.outcome==null)){ 
+                                return 1.5
+                                }
+								else {return 0}})
     
     var start_location = svg.append("g")
                             .selectAll("circle")
@@ -137,10 +155,19 @@ d3.json("18245_madrid.json").then(function(dataset){
 							.attr('cy', function(d){return y(d.location[1])})
                             .attr("fill", "#157f3b")
                             .attr("r", function(d){
-								if (d.minute<5 && (d.pass.outcome==null)){ 
+								if (d.minute==temp && (d.pass.outcome==null)){ 
 									return 10
 									}
 									else {return 0}})
+                            .on('mouseover', function(){
+                                d3.select(this)
+                                    .attr("stroke-width",0.5)
+                                    .attr("stroke","black")
+                            })
+                            .on('mouseout', function(){
+                                d3.select(this)
+                                    .attr("stroke-width", 0)
+                            })
                             
     
     var start_text = svg.append("g")
@@ -153,9 +180,9 @@ d3.json("18245_madrid.json").then(function(dataset){
                     .attr("dx", function(d){return x(d.location[0])})
                     .attr("dy", function(d){return y(d.location[1])})
                     .text(function(d){
-						if (d.minute<5  && (d.pass.outcome==null)){return jersey_numbers[d.player.id]}
+						if (d.minute==temp  && (d.pass.outcome==null)){return jersey_numbers[d.player.id]}
 						else {return null}})
-                    .attr('font-size', "20px")
+                    .attr('font-size', "15px")
                     .attr("transform", "translate(0,5)" )
     
     var end_location=svg.append("g")
@@ -167,11 +194,20 @@ d3.json("18245_madrid.json").then(function(dataset){
                         .attr('cy', function(d){return y(d.pass.end_location[1])})
                         .attr("fill",  "#2f7ebc")
                         .attr("r", function(d){
-                            if (d.minute<5 && (d.pass.outcome==null)){ 
+                            if (d.minute==temp && (d.pass.outcome==null)){ 
                                 return 10
                                 }
 								else {return 0}})
                         .attr("transform", "translate(0,-5)" )
+                        .on('mouseover', function(){
+                            d3.select(this)
+                                .attr("stroke-width",0.5)
+                                .attr("stroke","black")
+                        })
+                        .on('mouseout', function(){
+                            d3.select(this)
+                                .attr("stroke-width", 0)
+                        })
     
     
         var end_text = svg.append('g')
@@ -184,24 +220,9 @@ d3.json("18245_madrid.json").then(function(dataset){
                         .attr("dx",  function(d){return x(d.pass.end_location[0])})
                         .attr("dy",  function(d){return y(d.pass.end_location[1])})
                         .text(function(d){
-                            if (d.pass.recipient !=null && d.minute<5  && (d.pass.outcome==null)){return jersey_numbers[d.pass.recipient.id]}
+                            if (d.pass.recipient !=null && d.minute==temp  && (d.pass.outcome==null)){return jersey_numbers[d.pass.recipient.id]}
                             else{return null}
 							})
-                        .attr('font-size', "20px")
+                        .attr('font-size', "15px")
     
-        var edges = svg.append("g")
-                        .selectAll("line")
-                        .data(dataset)
-                        .enter()
-                        .append("line")
-                        .attr("x1", function(d){return x(d.location[0])})                           
-                        .attr("y1", function(d){return y(d.location[1])})
-                        .attr("x2", function(d){return x(d.pass.end_location[0])})
-                        .attr("y2", function(d){return y(d.pass.end_location[1])})
-                        .attr("stroke", "black")
-                        .attr("stroke-width", function(d){
-                            if (d.minute<5 && (d.pass.outcome==null)){ 
-                                return 2
-                                }
-								else {return 0}})
     })
---
+++ nodes.js
@@ -0,0 +1,71 @@
+d3.json("18245_madrid.json").then(function (dataset){
+
+var dimensions = {
+    width: 1350,
+    height: 400,
+    margin: {
+        top: 10,
+        bottom: 10,
+        right: 10,
+        left: 50}
+    }
+
+var jersey_numbers={5597 : 1, 5721 : 2, 5485 : 5, 5201 : 4, 5552 : 12, 5539 : 14, 5463 : 10, 5574 : 8, 4926: 22, 19677 : 9, 5207 : 7}
+
+var svg = d3.select("#nodes")
+//.append("svg")
+        .style("width", dimensions.width + dimensions.margin.left + dimensions.margin.right)
+        .style("height", dimensions.height + dimensions.margin.top + dimensions.margin.bottom)
+        .append("g")
+        //.attr("transform", "translate(" + dimensions.margin.left + "," + dimensions.margin.top + ")")
+
+    temp=d3.group(dataset, (d=>d.minute), (d=>d.player.id))
+    console.log(temp.get(11))
+
+    var pass_minute=d3.group(dataset, (d=>d.minute), (d=>d.player.id))
+
+    var xScale = d3.scaleBand()
+                    .domain(dataset.map(d=>d.minute)) //defines the interval of values in our dataset
+                    .range([dimensions.margin.left,dimensions.width-dimensions.margin.right]) //defines the pixels // starting from zero pixel and maximum value is what we have set for the graph at the top of script  
+                    .padding(0.2)
+    
+    var yScale = d3.scaleOrdinal()
+                    .domain(["1","2","5","4","12", "14", "10", "8", "22", "9", "7"])
+                    .range([dimensions.height-30,dimensions.height-60,dimensions.height-90, dimensions.height-120, dimensions.height-150,dimensions.height-180, dimensions.height-210,dimensions.height-240, dimensions.height-270, dimensions.height-300, dimensions.height-330])
+
+       console.log(temp.get(10).get(5721))
+       console.log(jersey_numbers[5721])
+
+    var bar = svg.selectAll("rect")
+                .data(pass_minute)
+                .enter()
+                .append("rect")
+                .attr("x", d => xScale(d[0]))
+                .attr("y", 0)
+                .attr("width", xScale.bandwidth())
+                .attr("height", dimensions.height - dimensions.margin.bottom)
+                .attr("fill", "steelblue")
+                .attr("opacity", "0.1")
+
+    var xAxis = svg.append("g")
+                   .call(d3.axisBottom().scale(xScale))
+                   .style("transform", `translateY(${dimensions.height - dimensions.margin.bottom}px)`)
+                   .selectAll("text")
+                   .style("text-anchor", "end")
+
+    var circles=svg.selectAll("circle")
+                   .data(dataset)
+                   .enter()
+                   .append("circle")
+                   .attr("x", d=>xScale(d.minute))
+                   .attr("y", d=>yScale(jersey_numbers[d.player.id]))
+                   .attr("r", 5)
+
+    var yAxis = svg.append("g")
+                    .call(d3.axisLeft().scale(yScale))
+                    .style("transform", `translateX(${dimensions.margin.left}px)`) 
+
+
+
+})
+    
