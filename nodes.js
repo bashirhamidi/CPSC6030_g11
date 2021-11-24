9c69efd702c6ee7c85e00f2b4dcd0ed0279b5e17
anoopkak <90483777+anoopkak@users.noreply.github.com>
2021-11-24 15:44:44 -0500

    Update nodes.js

--- nodes.js
+++ nodes.js
@@ -10,7 +10,7 @@ var dimensions = {
         left: 50}
     }
 
-var jersey_numbers={5597 : 1, 5721 : 2, 5485 : 5, 5201 : 4, 5552 : 12, 5539 : 14, 5463 : 10, 5574 : 8, 4926: 22, 19677 : 9, 5207 : 7}
+var jersey_numbers={5597 : 1, 5721 : 2, 5485 : 5, 5201 : 4, 5552 : 12, 5539 : 14, 5463 : 10, 5574 : 8, 4926: 22, 19677 : 9, 5207 : 7, 6399 : 11, 5202 : 6}
 
 var svg = d3.select("#nodes")
 //.append("svg")
@@ -20,7 +20,7 @@ var svg = d3.select("#nodes")
         //.attr("transform", "translate(" + dimensions.margin.left + "," + dimensions.margin.top + ")")
 
     temp=d3.group(dataset, (d=>d.minute), (d=>d.player.id))
-    console.log(temp.get(11))
+    console.log(temp)
 
     var pass_minute=d3.group(dataset, (d=>d.minute), (d=>d.player.id))
 
@@ -30,11 +30,11 @@ var svg = d3.select("#nodes")
                     .padding(0.2)
     
     var yScale = d3.scaleOrdinal()
-                    .domain(["1","2","5","4","12", "14", "10", "8", "22", "9", "7"])
+                    .domain(["1","2","5","4","12", "6", "14", "10", "8", "22", "9", "7", "11"])
                     .range([dimensions.height-30,dimensions.height-60,dimensions.height-90, dimensions.height-120, dimensions.height-150,dimensions.height-180, dimensions.height-210,dimensions.height-240, dimensions.height-270, dimensions.height-300, dimensions.height-330])
 
        console.log(temp.get(10).get(5721))
-       console.log(jersey_numbers[5721])
+       console.log(dataset)
 
     var bar = svg.selectAll("rect")
                 .data(pass_minute)
@@ -57,9 +57,16 @@ var svg = d3.select("#nodes")
                    .data(dataset)
                    .enter()
                    .append("circle")
-                   .attr("x", d=>xScale(d.minute))
-                   .attr("y", d=>yScale(jersey_numbers[d.player.id]))
+                   .attr("cx", d=>xScale(d.minute))
+                   .attr("cy", function(d){
+                       if ((d.team.name=="Real Madrid") && (d.pass.outcome==null)){
+                       const output=yScale(jersey_numbers[d.player.id])
+                       console.log(output)
+                       return output}
+                   })
                    .attr("r", 5)
+                   .attr("fill", "steelblue")
+
 
     var yAxis = svg.append("g")
                     .call(d3.axisLeft().scale(yScale))
