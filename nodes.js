cf42b66b870d004feb49cd3fad6ee60abfa94ee6
anoopkak <90483777+anoopkak@users.noreply.github.com>
2021-11-26 16:21:54 -0500

    Update nodes.js

--- nodes.js
+++ nodes.js
@@ -10,14 +10,14 @@ var dimensions = {
         left: 50}
     }
 
-var jersey_numbers={5597 : 1, 5721 : 2, 5485 : 5, 5201 : 4, 5552 : 12, 5539 : 14, 5463 : 10, 5574 : 8, 4926: 22, 19677 : 9, 5207 : 7, 6399 : 11, 5202 : 6}
+var jersey_numbers={5597 : 1, 5721 : 2, 5485 : 5, 5201 : 4, 5552 : 12, 5539 : 14, 5463 : 10, 5574 : 8, 4926: 22, 19677 : 9, 5207 : 7, 6399 : 11, 5202 : 6, 5719:20}
 
 var svg = d3.select("#nodes")
 //.append("svg")
         .style("width", dimensions.width + dimensions.margin.left + dimensions.margin.right)
         .style("height", dimensions.height + dimensions.margin.top + dimensions.margin.bottom)
         .append("g")
-        //.attr("transform", "translate(" + dimensions.margin.left + "," + dimensions.margin.top + ")")
+        .attr("transform", "translate(" + dimensions.margin.left + "," + dimensions.margin.top + ")")
 
     temp=d3.group(dataset, (d=>d.minute), (d=>d.player.id))
     console.log(temp)
@@ -29,12 +29,11 @@ var svg = d3.select("#nodes")
                     .range([dimensions.margin.left,dimensions.width-dimensions.margin.right]) //defines the pixels // starting from zero pixel and maximum value is what we have set for the graph at the top of script  
                     .padding(0.2)
     
-    var yScale = d3.scaleOrdinal()
-                    .domain(["1","2","5","4","12", "6", "14", "10", "8", "22", "9", "7", "11"])
-                    .range([dimensions.height-30,dimensions.height-60,dimensions.height-90, dimensions.height-120, dimensions.height-150,dimensions.height-180, dimensions.height-210,dimensions.height-240, dimensions.height-270, dimensions.height-300, dimensions.height-330])
+    var yScale = d3.scaleBand()
+                    .domain(["1","2","5","4","12", "6", "14", "10", "8", "22", "9", "7", "11","20"])
+                    .range([dimensions.height-dimensions.margin.bottom, dimensions.margin.top])
 
-       console.log(temp.get(10).get(5721))
-       console.log(dataset)
+       console.log(yScale("10"))
 
     var bar = svg.selectAll("rect")
                 .data(pass_minute)
@@ -59,20 +58,34 @@ var svg = d3.select("#nodes")
                    .append("circle")
                    .attr("cx", d=>xScale(d.minute))
                    .attr("cy", function(d){
-                       if ((d.team.name=="Real Madrid") && (d.pass.outcome==null)){
-                       const output=yScale(jersey_numbers[d.player.id])
-                       console.log(output)
-                       return output}
+                       if ((d.pass.outcome==null)||(d.team.name=="Real Madrid")){
+                       output=(jersey_numbers[d.player.id])}
+                      if (output==null){return -100}
+                      else{return yScale(""+output)}
+                    
                    })
                    .attr("r", 5)
                    .attr("fill", "steelblue")
-
+                   .attr("transform", "translate(6,13.5)")
 
     var yAxis = svg.append("g")
                     .call(d3.axisLeft().scale(yScale))
                     .style("transform", `translateX(${dimensions.margin.left}px)`) 
 
 
-
-})
+    var lines=svg.append("g")
+                 .selectAll("line")
+                 .data(["1","2","5","4","12", "6", "14", "10", "8", "22", "9", "7", "11","20"])
+                 .enter()
+                 .append("line")
+                .attr("x1", dimensions.margin.left)                           
+                .attr("y1", function(d){return yScale(d)})
+                .attr("x2", dimensions.width-dimensions.margin.right)
+                .attr("y2",  function(d){return yScale(d)})
+                .attr("stroke", "black")
+                .attr("stroke-width", 1)
+                .attr("transform", "translate(0,13.5)")
+                .attr("opacity", "0.5")
+ 
+                })
     
