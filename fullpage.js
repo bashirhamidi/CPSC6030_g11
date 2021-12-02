4f582e224e8d4f8c4e390cc4cee5ac4d9292f889
Mark B Hamidi <28765863+bashirhamidi@users.noreply.github.com>
2021-12-02 02:28:50 -0500

    Add files via upload
    updated headers and tags

--- fullpage.js
+++ fullpage.js
@@ -2,7 +2,8 @@ new fullpage('#FullPage', {
     autoScrolling: true,
     navigation: true,
     anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
-    navigationTooltips: ['Home', 'Background', 'About the visualization', 'Visualization', 'Contacts'],
+    navigationTooltips: ['Home', 'Background', 'About the dashboard', 'Dashboard', 'Contacts'],
     //showActiveTooltip: true,
-    scrollNavPosition: screenLeft
+    scrollNavPosition: screenLeft,
+    paddingTop: '0em'
 })
